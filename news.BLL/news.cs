907689f32fd85bd362415bcb102d602c25b75c04
mahan <mahan@DESKTOP-UI75Q1Q>
2022-05-16 19:18:13 +0430

    Add project files.

---
+++ news.BLL/news.cs
@@ -0,0 +1,19 @@
+﻿using news.DAL2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using news.Entities;
+
+namespace news.BLL2
+{
+    public class News
+    {
+        public khabar loadfirstnews()
+        {
+            newsDao newsDao = new newsDao();
+            return newsDao.Loadfirstnews();
+        }
+    }
+}
---
+++ news.DAL/newsDao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using news.Entities;
+
+namespace news.DAL2
+{
+    public class newsDao
+    {
+        public khabar Loadfirstnews()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=.;Initial Catalog=newsdata;"
+              + "Integrated Security=true;");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = "select id , textbox from text";
+            sqlCommand.CommandType = CommandType.Text;
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            reader.Read();
+            khabar kh = new khabar();
+            kh.textbox = reader[0].ToString();
+            kh.id = int.Parse(reader[1].ToString());
+
+
+
+            sqlConnection.Close();
+            return kh;
+        }
+    }
+
+}
